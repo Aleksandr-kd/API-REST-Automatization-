a0ed07483a21055ae7af3bdef3d18c1d31b485bc
Aleksandr <aleks_pochta_krd@mail.ru>
2025-09-12 16:19:18 +0300

    Редактирование кейсов

--- src/test/java/api/base/BaseService.java
+++ src/test/java/api/base/BaseService.java
@@ -12,8 +12,7 @@ import static io.restassured.RestAssured.given;
 
 public class BaseService {
 
-//    private static final String BASE_URL = "https://swift.techwithjatin.com";
-    private static final String BASE_URL = "https://swift.techwithjatin.com";
+    private static final String BASE_URL = System.getProperty("base.url");
     private final RequestSpecification requestSpecification;
 
     static
